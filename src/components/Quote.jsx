ca5237121b9ad0fd3a085a18d71b4843bcabfa59
Mohammad Sahil <sahilmejakhas@gmail.com>
2022-02-20 09:29:12 +0530

    Seocnd Quote

--- src/components/Quote.jsx
+++ src/components/Quote.jsx
@@ -4,7 +4,7 @@ import './Quote.css';
 function Quote() {
   return (
     <div className='Quote'>
-        <blockquote>When something is important enough, you do it even if the odds are not in your favor.</blockquote>
+        <blockquote>aaaaaa</blockquote>
                 <cite>Elon Musk</cite>
     </div>
   )
