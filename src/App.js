02529e433332c817617e0ed93b0593ec44e8f1cc
Mohammad Sahil <sahilmejakhas@gmail.com>
2022-03-02 00:37:04 +0530

    Quote added

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
     <Clock/>
     <Quote 
       key="1"
-      bquote = "“Get busy living or get busy dying.”"
+      bquote = "“I'm Anonymus.”"
       cite = "Stephen King"
     />
      {/* Header */}
