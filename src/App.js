62742a202f9f2eac823e9f157f7730cdd4def7d1
Mohammad Sahil <sahilmejakhas@gmail.com>
2022-06-05 23:07:58 +0530

    quote remed

--- src/App.js
+++ src/App.js
@@ -10,8 +10,8 @@ function App() {
     <Clock/>
     <Quote 
       key="1"
-      bquote = "Thiss iis jut  a quote"
-      cite = "Written by unknown"
+      bquote = "gfg"
+      cite = "jjsjjs"
     />
      {/* Header */}
      {/* Clock */}
