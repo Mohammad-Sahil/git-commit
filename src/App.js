fef29a2986c816d4191543623a4f9157da3365ea
Mohammad Sahil <sahilmejakhas@gmail.com>
2022-06-09 10:29:37 +0530

    quote added]

--- src/App.js
+++ src/App.js
@@ -10,8 +10,8 @@ function App() {
     <Clock/>
     <Quote 
       key="1"
-      bquote = "uerotueroteuo"
-      cite = "oiuioweucnvxcnmvncxroiweu"
+      bquote = "jksdjhfjhdf"
+      cite = "sahil"
     />
      {/* Header */}
      {/* Clock */}
