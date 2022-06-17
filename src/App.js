06cca682e4936c2d72e43b21c66feb0a657534b0
Mohammad Sahil <sahilmejakhas@gmail.com>
2022-06-17 11:36:00 +0530

    quote added of today

--- src/App.js
+++ src/App.js
@@ -10,8 +10,8 @@ function App() {
     <Clock/>
     <Quote 
       key="1"
-      bquote = "thsi is"
-      cite = "sahil"
+      bquote = " thsi is is is is  is"
+      cite = "is is is is is is is is is isi  is is is is is is is is"
     />
      {/* Header */}
      {/* Clock */}
